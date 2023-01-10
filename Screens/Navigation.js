556a0fec67feabc74ebe68c46aee65ea6e08c688
Parash Soni <parashsoni@Tushar.local>
2023-01-10 18:09:11 +0530

    first commit

---
+++ Screens/Navigation.js
@@ -0,0 +1,35 @@
+import React, { useRef } from "react";
+
+import { NavigationContainer } from "@react-navigation/native";
+import Tabs from "./Tabs";
+import { Animated, Dimensions, View } from "react-native";
+import { transform } from "@babel/core";
+
+
+const NavigationContain = () => {
+    const tabOffsetValue = useRef(new Animated.Value(0.0)).current;
+    return (
+        <NavigationContainer>
+            <Tabs />
+            <Animated.View style={{ 
+                width: getWidth(), 
+                height: 5, 
+                borderRadius: 5, 
+                backgroundColor: '#000000', 
+                position: "absolute", 
+                bottom: 55,
+                transform: [
+                    {translateX: tabOffsetValue}
+                ]}}>
+            </Animated.View>
+        </NavigationContainer>
+    );
+
+    function getWidth(){
+        let width = Dimensions.get("window").width
+        width = width - 40
+        return width/5
+    }
+}
+
+export default NavigationContain;
